ebd72ca05ffd18c11028d3c43be9ab306541f8da
João Pedro Lazarim de Souza <jplazarim@gmail.com>
2017-11-08 16:54:32 -0200

    Add files via upload

---
+++ loja_alunos/classes/Produto.php
@@ -0,0 +1,25 @@
+<?php
+
+class Produto
+{
+    private $codigo;
+    private $nome;
+    private $preco;
+    private $categoria;
+
+    public function cadastrar()
+    {
+        
+    }
+
+    public function atualizar()
+    {
+        
+    }
+
+    public function excluir()
+    {
+        
+    }
+    
+}
